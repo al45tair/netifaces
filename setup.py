f2a554c1376f26988625d34bd63303fb484548a5
Alastair Houghton <alastair@alastairs-place.net>
2014-04-28 17:58:02 +0100

    Bump version number and make the build status image into a link.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info[0] == 2:
 else:
     output = getattr(__builtins__, 'print')
     
-__version__ = "0.8"
+__version__ = "0.9"
 
 # Disable hard links, otherwise building distributions fails on OS X
 try:
