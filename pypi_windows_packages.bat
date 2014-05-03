1596cfe452338d610153a6f9031c5876b8659d7a
Oliver Palmer <oliverpalmer@opalmer.com>
2014-05-03 15:52:21 -0400

    adding Windows batch script used to build and upload multiple binary formats for Windows to PyPi

---
+++ pypi_windows_packages.bat
@@ -0,0 +1,4 @@
+C:\Python26\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel upload
+C:\Python27\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel upload
+C:\Python33\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel upload
+C:\Python34\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel upload
