d3531a51f336e19f0dd2c54102ee79078fd16623
Alastair Houghton <alastair@alastairs-place.net>
2014-05-19 12:39:50 +0100

    Bumped version numbers.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info[0] == 2:
 else:
     output = getattr(__builtins__, 'print')
     
-__version__ = "0.10.3"
+__version__ = "0.10.4"
 
 # Disable hard links, otherwise building distributions fails on OS X
 try:
