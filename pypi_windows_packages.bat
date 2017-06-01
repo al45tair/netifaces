86343c26c65e396b7818906c22483fed550a4af2
Alastair Houghton <alastair@alastairs-place.net>
2017-06-01 14:23:35 +0100

    Updated Windows packaging batch file.

--- pypi_windows_packages.bat
+++ pypi_windows_packages.bat
@@ -1,4 +1,6 @@
-C:\Python26\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel upload
-C:\Python27\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel upload
-C:\Python33\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel upload
-C:\Python34\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel upload
+C:\Python27\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel
+C:\Python34\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel
+C:\Python35\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel
+C:\Python27_32\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel
+C:\Python34_32\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel
+C:\Python35_32\python.exe setup.py clean bdist_egg bdist_wininst bdist_wheel
